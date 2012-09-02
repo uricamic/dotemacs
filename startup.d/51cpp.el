b4ebeda4a6535ea1d7ff625be7b50ad5664e7076
K0stIa <antonjuk@gmail.com>
2012-09-02 15:09:36 +0200

    some cpp fix (don't know)

--- startup.d/51cpp.el
+++ startup.d/51cpp.el
@@ -38,9 +38,7 @@
 )
 
 
-;;;;;;;;;;;;;;;;;;;;;;
-(setq c-default-style "linux" c-basic-offset 4)
-
-(require 'google-c-style)
-
-(add-hook 'c-mode-common-hook 'google-set-c-style)
+(defun my-c++-mode-hook ()
+  (setq c-basic-offset 4)
+  (c-set-offset 'substatement-open 0))
+(add-hook 'c++-mode-hook 'my-c++-mode-hook)
