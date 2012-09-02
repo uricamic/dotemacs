fe8b720bc8df32e955c61cac154befe55c716898
K0stIa <antonjuk@gmail.com>
2012-09-02 15:09:14 +0200

    yellow color is not shown as a tab

--- startup.d/50whitespace.el
+++ startup.d/50whitespace.el
@@ -1,6 +1,10 @@
 (require 'whitespace)
-(set 'whitespace-style '(trailing tabs lines-tail indentation::space face))
-(setq whitespace-global-modes
-      '(c-mode c++-mode emacs-lisp-mode
-               python-mode lisp-mode))
+;(set 'whitespace-style '(trailing tabs lines-tail indentation::space face))
+;(setq whitespace-global-modes
+;      '(c-mode c++-mode emacs-lisp-mode
+;               python-mode lisp-mode))
+
+(setq whitespace-style
+  '(empty lines-tail tabs tab-mark trailing))
+
 (global-whitespace-mode 1)
