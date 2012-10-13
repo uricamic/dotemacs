295b30c1a93d5653b1e78e7a2a3449310c8ec8f2
K0stIa <antonjuk@gmail.com>
2012-10-13 12:11:40 +0200

    path typo fixed

--- configs/slime.el
+++ configs/slime.el
@@ -1,5 +1,5 @@
 (setq inferior-lisp-program (executable-find "clisp"))
-(add-to-list 'load-path "~/.emacs.d/vendor/slime")
+(add-to-list 'load-path "~/.emacs.d/3dparty/slime")
 (setq slime-lisp-implementations
       `((common-lisp (,(executable-find "clisp")))))
 (require 'slime-autoloads)
