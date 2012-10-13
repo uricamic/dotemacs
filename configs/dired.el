c9055ff45756a55c6603de9f25f967cf66eddbcd
K0stIa <antonjuk@gmail.com>
2012-10-13 15:36:51 +0200

    better solution for a single dired mode

--- configs/dired.el
+++ configs/dired.el
@@ -15,6 +15,23 @@
             (local-set-key "E" 'dired-gnome-open-file)))
 
 ;; single dired buffer
-(eval-after-load 'dired '(progn (require 'joseph-single-dired)))
+;;(eval-after-load 'dired '(progn (require 'joseph-single-dired)))
+(add-hook 'dired-mode-hook
+ (lambda ()
+  (define-key dired-mode-map (kbd "<return>")
+    'dired-find-alternate-file) ; was dired-advertised-find-file
+  (define-key dired-mode-map (kbd "^")
+    (lambda () (interactive) (find-alternate-file "..")))
+  ; was dired-up-directory
+ ))
 ;; instill dired+
 (require 'dired+)
+
+
+;; allow dired to be able to delete or copy a whole dir.
+;; “always” means no asking. “top” means ask once. Any other symbol means ask each and every time for a dir and subdir.
+(setq dired-recursive-copies (quote always))
+(setq dired-recursive-deletes (quote top))
+
+;; copy from one dired dir to the next dired dir shown in a split window
+(setq dired-dwim-target t)
